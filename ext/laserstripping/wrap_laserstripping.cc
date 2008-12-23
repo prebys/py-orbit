83782591077173d8eb6a5c733933f673c5e6f2dc
tg4 <tg4@34b46b51-e68d-51de-b4c4-160ac875e1d8>
2008-12-23 18:11:22 +0000

    development of LaserField class

--- ext/laserstripping/wrap_laserstripping.cc
+++ ext/laserstripping/wrap_laserstripping.cc
@@ -2,6 +2,7 @@
 
 #include "wrap_las_strip_external_effects.hh"
 #include "wrap_cpp_base_field_source.hh"
+#include "wrap_gaussian_laser_field.hh"
 
 static PyMethodDef laserStrippingMethods[] = { {NULL,NULL} };
 
@@ -14,10 +15,10 @@ extern "C" {
     PyObject* module = Py_InitModule("laserstripping",laserStrippingMethods);
 		wrap_laserstripping_las_strip_external_effects::initLasStripExternalEffects(module);
 		wrap_laserstripping_cpp_base_field_source::initCppBaseFieldSource(module);
+		wrap_gasussian_laser_field::initGaussianLaserField(module);
 
   }
 	
 #ifdef __cplusplus
 }
 #endif
-
