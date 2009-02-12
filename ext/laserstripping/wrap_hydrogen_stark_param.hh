c609b99dbbf9daa822aaf50fdc91ea3267935d34
tg4 <tg4@34b46b51-e68d-51de-b4c4-160ac875e1d8>
2009-02-12 21:33:25 +0000

    adding new wrapers

---
+++ ext/laserstripping/wrap_hydrogen_stark_param.hh
@@ -0,0 +1,20 @@
+#ifndef WRAP_HYDROGEN_STARK_PARAM_HH_
+#define WRAP_HYDROGEN_STARK_PARAM_HH_
+
+
+#include "Python.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  namespace wrap_hydrogen_stark_param{
+    void initHydrogenStarkParam(PyObject* module);
+  }
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /*WRAP_HYDROGEN_STARK_PARAM_HH_*/
