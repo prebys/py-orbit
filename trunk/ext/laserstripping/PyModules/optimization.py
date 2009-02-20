4c2f966d9d846471e5dc522635b478f3b5903e82
tg4 <tg4@34b46b51-e68d-51de-b4c4-160ac875e1d8>
2009-02-20 15:55:30 +0000

    files arranged by inner directories

--- trunk/ext/laserstripping/PyModules/optimization.py
+++ trunk/ext/laserstripping/PyModules/optimization.py
@@ -2,9 +2,9 @@
 #Track bunch with r and p through the external field 
 # The field is 1 T and has direction (0,1,0)
 #-----------------------------------------------------
-addr="./ext/laserstripping/"
+
 import sys
-sys.path.append(addr+"/PyModules/")
+sys.path.append("/home/tg4/workspace/PyOrbit/ext/laserstripping/PyModules/")
 import math
 
 from bunch import *
@@ -17,14 +17,16 @@ import orbit_mpi
 import random
 import part_generator
 
-print "Start."
+from orbit_mpi import mpi_comm,mpi_datatype,mpi_op
 
+rank = orbit_mpi.MPI_Comm_rank(mpi_comm.MPI_COMM_WORLD)
 
+if(rank == 0): print "Start."
 
-
+random.seed((rank+1)*1257)
 
 z0=2.0e-3
-Bx=1./10000.
+Bx=0.1/10000.
 n_states=3
 TK = 1.0
 #fx=fy=-0.1
@@ -41,11 +43,11 @@ N_part = 1
 
 alphaX = -3.294               # [rad]
 betaX = 26.7                  # [m]
-emtX = 0.225e-6/(0.736*0.736) # [m*rad]
+emtX = 0.225e-6               # [m*rad]
 
 alphaY = 1.979            # [rad]
 betaY = 3.906             # [m]
-emtY = 3.803e-6/(3.5*3.5) # [m*rad]
+emtY = 0.380e-6           # [m*rad]
 
 relativeSpread = 1.0e-4
 
@@ -60,77 +62,107 @@ trGenX = part_generator.TransverseCoordGen(alphaX,betaX,emtX,cutOffX)
 cutOffY = math.sqrt(emtY*betaY)*3.0
 trGenY = part_generator.TransverseCoordGen(alphaY,betaY,emtY,cutOffY)
 
-#print "cutOffX= %6.2f cutOffY= %6.2f "%(cutOffX*1.0e+3,cutOffY*1.0e+3)
+if(rank == 0): print "cutOffX= %6.2f cutOffY= %6.2f "%(cutOffX*1.0e+3,cutOffY*1.0e+3)
 
 pGen = part_generator.EnergyGen(TK,relativeSpread)
-
 
 partGen = part_generator.ParticlesGen(dispD,dispDP,trGenX,trGenY,pGen)
 
 
-bunch = Bunch()
 bunch_target = Bunch()
-bunch.charge(0)
 bunch_target.charge(0)
-bunch.addPartAttr("Amplitudes")
-for i in range(N_part):
-    (x,px,y,py,z,pz) = partGen.getCoords()
-    bunch.addParticle(x,px,y,py,-z0,pz)
-    bunch.partAttrValue("Amplitudes",i,1,1.0)
+bunch_target.mass(0.938256 + 0.000511)
+bunch_target.addPartAttr("Amplitudes")
 
-#two bunches should have the same particle attributes to copy operation
-
-
-E = bunch.mass() + TK
-P = math.sqrt(E*E - bunch.mass()*bunch.mass())
+E = bunch_target.mass() + TK
+P = math.sqrt(E*E - bunch_target.mass()*bunch_target.mass())
 vz=299792458*P/E
 time_step = (2*z0/vz)/n_step
 
 
 
-Stark=HydrogenStarkParam(addr+"/transitions/",n_states)
+
+av=0;
+nstat=100000
+for i in range(nstat):
+    (x,px,y,py,z,pz) = partGen.getCoords()
+    av+=pz
+    
+av=av/nstat
+
+
+av2=0
+for i in range(nstat):
+    (x,px,y,py,z,pz) = partGen.getCoords()
+    av2+=px*px
+    
+av2=math.sqrt(av2/nstat)
+
+print av2/av
+sys.exit(1)
+
+
+
+
+Stark=HydrogenStarkParam("/home/tg4/workspace/PyOrbit/ext/laserstripping/transitions/",n_states)
 
 #print alpha
 #print kz
 #sys.exit(1)
 
+if(rank == 0):
+	f_out = open("/home/tg4/workspace/PyOrbit/result_ls.dat","w")
+	f_out.write("cpu_time   W [MW]     wx [um]   dist[cm]   Population \n")
+	f_out.close()
+	print "======START LOOP=========="
+
+
+time_start = orbit_mpi.MPI_Wtime()
 
 for power,wx,fxy in [(power,wx,fxy) 
                     for power in [1.0e6,1.5e6,2.0e6] 
                     for wx in [60.0e-6 ,80.0e-6,100.0e-6,120.0e-6,140.0e-6,160.0e-6,180.0e-6,200.0e-6] 
                     for fxy in [-0.00,-0.02,-0.04,-0.06,-0.08,-0.1,-0.12,-0.14,-0.16,-0.18,-0.2]]:
-
-	
-	bunch_target.deleteAllParticles()
-	bunch.copyBunchTo(bunch_target)
-
-	la0= 2*math.pi*5.291772108e-11/7.297352570e-3/(
-		Stark.getStarkEnergy(bunch_target.mass(),   0,2,0,  0.,0.,0.,  Bx,0.,0.,   0.,0.,P)
-		-Stark.getStarkEnergy(bunch_target.mass(),   0,0,0,  0.,0.,0.,  Bx,0.,0.,   0.,0.,P))
-	
-	kz=-1/math.sqrt(math.pow(P/(bunch_target.mass()*(la/la0-1)-TK),2)-1)
-    
-#    alpha=360*math.acos((b.mass()*(la/la0-1)-TK)/P)/2/math.pi
-#    print alpha
-
-	fx=fy=fxy
-	LFS=HermiteGaussianLFmode(math.sqrt(power),0,0,wx,wy,fx,fy,la) 
-	LFS.setLaserFieldOrientation(0.,0.,0.,   -1.,0.,kz,   1.,0.,1./kz,  0.,1.,0.)
-
-	tracker = RungeKuttaTracker(1000.0)
-	First = LasStripExternalEffects(LFS,Stark,10000.)
-	fS=LSFieldSource(0.,0.,0.,Bx,0.,0.)
-	tracker.track(bunch_target,0,time_step*n_step, time_step,fS,First)
-	
 	population = 0.
-	for i in range(bunch_target.getSize()):
-		population += (1-bunch_target.partAttrValue("Amplitudes",i,1))
-	population = population/bunch_target.getSize()
-	#file=open("020_tuning.txt",'a')
-	#file.write('%f'%Bx+"\t")
-	#file.write('%f'%population+"\n")
-	#file.close() 
-	bunch_target.deleteAllParticles()
-	print  "W = %4.1f  wx [um] = %4.1f  dist[cm]= %4.1f  Population: %7.3f "%(power,1.0e+6*wx,fxy*100,population)
+	for i in range(N_part):
+		bunch_target = Bunch()
+		bunch_target.charge(0)
+		bunch_target.mass(0.938256 + 0.000511)
+		bunch_target.addPartAttr("Amplitudes")		
+		(x,px,y,py,z,pz) = partGen.getCoords()
+		bunch_target.addParticle(0.,0.,0.,0.,-z0,pGen.getP0())
+		#bunch_target.addParticle(x,px,y,py,z,pz)
+		bunch_target.partAttrValue("Amplitudes",0,1,1.0)
 
+		la0= 2*math.pi*5.291772108e-11/7.297352570e-3/(Stark.getStarkEnergy(bunch_target.mass(),   0,2,0,  0.,0.,0.,  Bx,0.,0.,   0.,0.,P) -Stark.getStarkEnergy(bunch_target.mass(),   0,0,0,  0.,0.,0.,  Bx,0.,0.,   0.,0.,P))
+	
+		kz=-1/math.sqrt(math.pow(P/(bunch_target.mass()*(la/la0-1)-TK),2)-1)
+    
+		#       alpha=360*math.acos((b.mass()*(la/la0-1)-TK)/P)/2/math.pi
+		#       print alpha
+		fx=fy=fxy
+		LFS=HermiteGaussianLFmode(math.sqrt(power),0,0,wx,wy,fx,fy,la) 
+		LFS.setLaserFieldOrientation(0.,0.,0.,   -1.,0.,kz,   1.,0.,1./kz,  0.,1.,0.)
+
+		tracker = RungeKuttaTracker(1000.0)
+		First = LasStripExternalEffects(LFS,Stark,10000.)
+		fS=LSFieldSource(0.,0.,0.,Bx,0.,0.)
+		print "debug  ===track ========== rank=",rank," starkEnrg=",Stark.getStarkEnergy(bunch_target.mass(),   0,2,0,  0.,0.,0.,  Bx,0.,0.,   0.,0.,P)
+		tracker.track(bunch_target,0,time_step*n_step, time_step,fS,First)
+		print "debug  amp=",(1-bunch_target.partAttrValue("Amplitudes",0,1))
+		population += (1-bunch_target.partAttrValue("Amplitudes",0,1))
+
+	population = population/N_part 	
+	mpi_size = orbit_mpi.MPI_Comm_size(mpi_comm.MPI_COMM_WORLD)
+	op = mpi_op.MPI_SUM
+	data_type = mpi_datatype.MPI_DOUBLE
+	population = orbit_mpi.MPI_Allreduce(population,data_type,op,mpi_comm.MPI_COMM_WORLD)	
+	population = population/mpi_size
+	time_live = orbit_mpi.MPI_Wtime() - time_start
+	res = " %6.0f  %4.1f  %4.1f  %4.1f    %7.3f "%(time_live,power/1.0e+6,1.0e+6*wx,fxy*100,population)
+	if(rank == 0): print  "W [MW]= %4.1f  wx [um] = %4.1f  dist[cm]= %4.1f  Population: %7.3f "%(power/1.0e+6,1.0e+6*wx,fxy*100,population)
+	if(rank == 0):
+		f_out = open("/home/tg4/workspace/PyOrbit/result_ls.dat","a")
+		f_out.write(res + "\n")
+		f_out.close()
 
