db5e4b5d813c0cf338b088a77df10814541d8d56
tg4 <tg4@34b46b51-e68d-51de-b4c4-160ac875e1d8>
2009-05-01 20:14:20 +0000

    New class for computation of density matrix without laser field interaction is created. System of equation in this case is simple and includes only diagonal density matrix elements.

---
+++ trunk/ext/laserstripping/DM_noLaserField/wrap_DM_noLaserField.hh
@@ -0,0 +1,21 @@
+#ifndef WRAP_DM_NOLASERFIELD_HH_
+#define WRAP_DM_NOLASERFIELD_HH_
+
+#include "Python.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  namespace wrap_DM_noLaserField{
+    void initDM_noLaserField(PyObject* module);
+  }
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*WRAP_DM_NOLASERFIELD_HH_*/
+
+
+
